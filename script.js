fc4d44ca8f4ccf7620cbc59112d64b954bb10cdc
Wang Dani <daniprogger@outlook.com>
2020-10-08 23:58:11 +0800

    Version 1.0
    Supports both comment and repost, as well as updated uid retrieval.

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 // ==UserScript==
 // @name         微博WAP版助手
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  辅助添加按钮到页面，方便打开微博评论页面电脑版
+// @version      1.0
+// @description  辅助添加按钮到页面，方便打开单条微博页面电脑版
 // @author       andriasw, snomiao
 // @match        https://weibo.cn/comment/*
+// @match        https://weibo.cn/repost/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    var url=window.location.href;
-    if (/\?uid/.test(url)) {
-
-        var urlPC = window.location.href.replace('comment', url.match(/\?uid=(\d+)/)[1]).replace('weibo.cn', 'weibo.com');
-
-        //thx snomiao
-
+    let btnPC = (link) => { // thx snomiao
         var a = document.createElement("a");
-        a.innerText="PC版"; a.classList.add('nl'); a.href=urlPC;
+        a.innerText="PC版"; a.classList.add('nl'); a.href=link;
         var txt = document.createTextNode("|");
         document.querySelector('.n').children[4].parentElement.appendChild(txt)
         document.querySelector('.n').children[4].parentElement.appendChild(a)
     }
 
+    var uid = document.getElementById('M_').outerHTML.match(/\?uid=(\d+)/)[1];
+    var urlPC = window.location.href.replace(/(comment|repost)/, uid).replace('weibo.cn', 'weibo.com');
+    btnPC(urlPC)
 
 })();
