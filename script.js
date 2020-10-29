08c69913728c836343dd91b4c2c5e03654f71cc4
Wang Dani <daniprogger@outlook.com>
2020-10-29 10:42:58 +0800

    Update script.js
    添加个人主页、我的首页跳转链接
    添加区分转发与评论的链接尾巴

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
 // ==UserScript==
 // @name         微博WAP版助手
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  辅助添加按钮到页面，方便打开单条微博页面电脑版
+// @version      1.1
+// @description  从WAP版微博页面和个人主页跳转到PC版
 // @author       andriasw, snomiao
-// @match        https://weibo.cn/comment/*
-// @match        https://weibo.cn/repost/*
+// @match        https://weibo.cn/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    let btnPC = (link) => { // thx snomiao
+    var btnPC = (link) => { // thx snomiao
         var a = document.createElement("a");
         a.innerText="PC版"; a.classList.add('nl'); a.href=link;
         var txt = document.createTextNode("|");
         document.querySelector('.n').children[4].parentElement.appendChild(txt)
         document.querySelector('.n').children[4].parentElement.appendChild(a)
+    } // 添加PC版按钮
+
+    //判断页面类型：我的首页、个人主页和微博页面
+    var hmpg, profilePage, weiboPage = 0;
+    if (document.title === '我的首页') hmpg =1;
+    else if (document.getElementsByClassName('ut').length) profilePage = 1; //https://www.techiedelight.com/check-element-exists-with-given-class-javascript/
+    else if (document.getElementById('M_') !== null) weiboPage = 1;
+
+    var urlPC='https://weibo.com/';
+
+    if(hmpg); //转到首页，PC版链接不变
+    else if(profilePage) {
+        var uid = document.getElementsByClassName('ut')[0].innerHTML.match(/\?uid=(\d+)/)[1];
+        urlPC += 'u/'+uid;
+    }
+    else if(weiboPage) {
+        var uid2 = document.getElementById('M_').outerHTML.match(/\?uid=(\d+)/)[1];
+        var weiboID = window.location.href.match(/\/(comment|repost)\/(\w+)(\/|\?)?/)[2];
+
+        var isComment=window.location.href.indexOf("/comment/")!=-1;
+        var isRepost=window.location.href.indexOf("/repost/")!=-1;
+        var tail;
+        if(isComment) tail='?type=comment';
+        else if(isRepost) tail='?type=repost';
+
+        urlPC += uid2+'/'+weiboID+tail;
     }
 
-    var uid = document.getElementById('M_').outerHTML.match(/\?uid=(\d+)/)[1];
-    var urlPC = window.location.href.replace(/(comment|repost)/, uid).replace('weibo.cn', 'weibo.com');
     btnPC(urlPC)
 
 })();
