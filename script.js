278e3400e56a04a67135da65a7b0a89e46036e28
Wang Dani <daniprogger@outlook.com>
2020-10-29 12:05:50 +0800

    Slight Rewrite
    1. Removed tail for comment page (because that is the default).
    2. Used one-line question mark statement for tail to repost, hence some long lines but still couldn't eliminate if-else.
    3. Added comments for more readable code.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         微博WAP版助手
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.1.2 short tail
 // @description  从WAP版微博页面和个人主页跳转到PC版
 // @author       andriasw, snomiao
 // @match        https://weibo.cn/*
@@ -11,38 +11,24 @@
 (function() {
     'use strict';
 
-    var btnPC = (link) => { // thx snomiao
+    const btnPC = (link) => { // thx snomiao
         var a = document.createElement("a");
         a.innerText="PC版"; a.classList.add('nl'); a.href=link;
         var txt = document.createTextNode("|");
         document.querySelector('.n').children[4].parentElement.appendChild(txt)
         document.querySelector('.n').children[4].parentElement.appendChild(a)
-    } // 添加PC版按钮
-
-    //判断页面类型：我的首页、个人主页和微博页面
-    var hmpg, profilePage, weiboPage = 0;
-    if (document.title === '我的首页') hmpg =1;
-    else if (document.getElementsByClassName('ut').length) profilePage = 1; //https://www.techiedelight.com/check-element-exists-with-given-class-javascript/
-    else if (document.getElementById('M_') !== null) weiboPage = 1;
+    } // Add link on page to PC version
 
     var urlPC='https://weibo.com/';
+    var uidRe=/\?uid=(\d+)/;
 
-    if(hmpg); //转到首页，PC版链接不变
-    else if(profilePage) {
-        var uid = document.getElementsByClassName('ut')[0].innerHTML.match(/\?uid=(\d+)/)[1];
-        urlPC += 'u/'+uid;
-    }
-    else if(weiboPage) {
-        var uid2 = document.getElementById('M_').outerHTML.match(/\?uid=(\d+)/)[1];
-        var weiboID = window.location.href.match(/\/(comment|repost)\/(\w+)(\/|\?)?/)[2];
-
-        var isComment=window.location.href.indexOf("/comment/")!=-1;
-        var isRepost=window.location.href.indexOf("/repost/")!=-1;
-        var tail;
-        if(isComment) tail='?type=comment';
-        else if(isRepost) tail='?type=repost';
-
-        urlPC += uid2+'/'+weiboID+tail;
+    // Homepage, profile page or specific Weibo page?
+    if(document.title === '我的首页'); // Do nothing if on homepage.
+    else if(document.getElementsByClassName('ut').length) urlPC += 'u/'+document.getElementsByClassName('ut')[0].innerHTML.match(uidRe)[1]; //+='u/'+uid
+    // The class 'ut' also appears in the homepage, hence line 26 to exclude homepage first.
+    else if(document.getElementById('M_') !== null) { // The corresponding element that contains uid...
+        var tail = (window.location.href.indexOf("/repost/")!=-1)?'?type=repost':''; // Add 'repost' tail if is a repost page. Do nothing if not.
+        urlPC += document.getElementById('M_').outerHTML.match(uidRe)[1]+'/'+window.location.href.match(/\/(comment|repost)\/(\w+)(\/|\?)?/)[2]+tail; // +=uid+weiboID+tail
     }
 
     btnPC(urlPC)
