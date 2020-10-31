ad8823c8b60eebe9413400abe1520047f62b1600
Wang Dani <daniprogger@outlook.com>
2020-10-31 20:38:49 +0800

    Add touch version support
    Touch version is now availble for sharing without login.
    
    And slight rewrite too.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         微博WAP版助手
 // @namespace    http://tampermonkey.net/
-// @version      1.2.1 touch
+// @version      1.2.3 touch
 // @description  从微博WAP版跳转到PC版或触屏版
 // @author       andriasw, snomiao
 // @match        https://weibo.cn/*
@@ -11,26 +11,26 @@
 (function() {
     'use strict';
 
-    var url=window.location.href;
-    var urlPC='https://weibo.com/';
-    var urlM='https://m.weibo.cn/';
-
-    const btn = (link=urlPC, m=urlM) => { // thx snomiao
+    const a = (text, link) => { // thx snomiao
         var a = document.createElement("a");
-        a.innerText="PC版"; a.classList.add('nl'); a.href=link;
+        a.innerText=text; a.classList.add('nl'); a.href=link;
         var txt = document.createTextNode("|");
         document.querySelector('.n').children[4].parentElement.appendChild(txt)
         document.querySelector('.n').children[4].parentElement.appendChild(a)
+    }
+    
+    var urlPC='https://weibo.com/';
+    var urlM='https://m.weibo.cn/';
 
-        var ma = document.createElement("a");
-        ma.innerText="触屏版"; ma.classList.add('nl'); ma.href=m;
-        var txtM = document.createTextNode("|");
-        document.querySelector('.n').children[5].parentElement.appendChild(txtM)
-        document.querySelector('.n').children[5].parentElement.appendChild(ma)
+    const btn = (link=urlPC, m=urlM, repost=0) => {
+        var tailPC = (repost)?'?type=repost':'';
+        var tailM = (repost)?'#repost':'';
+        a("PC版", link+tailPC)
+        a("触屏版", m+tailM)
     } // Add link on page to PC version
 
     // Homepage, profile page or specific Weibo page?
-    if(document.title === '我的首页') btn(); // Do nothing if on homepage.
+    if(document.title === '我的首页') btn(); // Use default if on homepage.
     else {
         const uidRe=/\?uid=(\d+)/;
 
@@ -40,10 +40,10 @@
         }
 
         else if(document.getElementById('M_') !== null) { // The corresponding element that contains uid...
+            const url=window.location.href;
             const weiboID = url.match(/\/(comment|repost)\/(\w+)(\/|\?)?/)[2];
             const uid = document.getElementById('M_').innerHTML.match(uidRe)[1]+'/';
-            if(url.indexOf("/repost/")!=-1) btn(urlPC+uid+weiboID+'?type=repost', urlM+'status/'+weiboID+'#repost');
-            else btn(urlPC+uid+weiboID, urlM+'status/'+weiboID);
+            btn(urlPC+uid+weiboID, urlM+'status/'+weiboID, url.indexOf("/repost/")!=-1);
         }
     }
 
