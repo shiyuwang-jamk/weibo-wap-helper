60735b7f15fe9f2dd437f1903305a445377f4149
Wang Dani <daniprogger@outlook.com>
2021-06-02 18:56:19 +0800

    Update script.js
    微博WAP版顶栏撤销了“搜索”和“话题”按钮，故更新以适应新顶栏。

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         微博WAP版助手
 // @namespace    http://tampermonkey.net/
-// @version      1.2.3 touch
+// @version      1.3
 // @description  从微博WAP版跳转到PC版或触屏版
 // @author       andriasw, snomiao
 // @match        https://weibo.cn/*
@@ -11,16 +11,17 @@
 (function() {
     'use strict';
 
+    var url=window.location.href;
+    var urlPC='https://weibo.com/';
+    var urlM='https://m.weibo.cn/';
+
     const a = (text, link) => { // thx snomiao
         var a = document.createElement("a");
         a.innerText=text; a.classList.add('nl'); a.href=link;
         var txt = document.createTextNode("|");
-        document.querySelector('.n').children[4].parentElement.appendChild(txt)
-        document.querySelector('.n').children[4].parentElement.appendChild(a)
+        document.querySelector('.n').children[2].parentElement.appendChild(txt)
+        document.querySelector('.n').children[2].parentElement.appendChild(a)
     }
-    
-    var urlPC='https://weibo.com/';
-    var urlM='https://m.weibo.cn/';
 
     const btn = (link=urlPC, m=urlM, repost=0) => {
         var tailPC = (repost)?'?type=repost':'';
@@ -30,17 +31,16 @@
     } // Add link on page to PC version
 
     // Homepage, profile page or specific Weibo page?
-    if(document.title === '我的首页') btn(); // Use default if on homepage.
+    if(document.title === '我的首页') btn(); // Do nothing if on homepage.
     else {
         const uidRe=/\?uid=(\d+)/;
 
         if(document.getElementsByClassName('ut').length) {
-            const uid = 'u/'+document.getElementsByClassName('ut')[0].innerHTML.match(uidRe)[1]; // The class 'ut' also appears in the homepage, hence line 33 to exclude homepage first.
+            const uid = 'u/'+document.getElementsByClassName('ut')[0].innerHTML.match(uidRe)[1]; // The class 'ut' also appears in the homepage, hence line 34 to exclude homepage first.
             btn(urlPC+uid, urlM+uid);
         }
 
         else if(document.getElementById('M_') !== null) { // The corresponding element that contains uid...
-            const url=window.location.href;
             const weiboID = url.match(/\/(comment|repost)\/(\w+)(\/|\?)?/)[2];
             const uid = document.getElementById('M_').innerHTML.match(uidRe)[1]+'/';
             btn(urlPC+uid+weiboID, urlM+'status/'+weiboID, url.indexOf("/repost/")!=-1);
